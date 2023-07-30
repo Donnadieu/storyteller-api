b6abf39a2a393f44595ec0ff2cbd7e0e7137b95d
Uche Chilaka <uche@lar.city>
2023-07-30 15:34:43 -0400

    refactor: WIP

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,13 @@ Rails.application.routes.draw do
       post "google", controller: :google, action: :create
       get "google", controller: :google, action: :index
     end
+
+    constraints HasRestrictedAccessConstraint do
+      mount Flipper::Api.app(Flipper) => '/flipper' # Docs: https://www.flippercloud.io/docs/api
+    end
+  end
+
+  scope :admin, as: :admin, constraints: HasRestrictedAccessConstraint do
+    mount Flipper::UI.app(Flipper) => '/flipper' # Docs: https://www.flippercloud.io/docs/api
   end
 end
