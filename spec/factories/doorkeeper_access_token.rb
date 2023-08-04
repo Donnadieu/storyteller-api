cc60458a52e7ee451a89777dc899b916cbf5ec44
quassdy <donnadieu.86@gmail.com>
2023-08-04 00:26:29 -0400

    STORYAI-2:Added factories

---
+++ spec/factories/doorkeeper_access_token.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :doorkeeper_access_token, class: 'Doorkeeper::AccessToken' do
+    application
+    resource_owner_id { FactoryBot.create(:user).id }
+    scopes { 'public' }
+    expires_in { 2.hours }
+    use_refresh_token { true }
+  end
+end
