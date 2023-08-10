95289f9d3adaa1b1c1649fa2486d0361ee9299e3
Uche Chilaka <uche@lar.city>
2023-08-10 07:25:28 -0400

    refactor: handling request errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  rescue_from Errors::Unauthorized,
+              Errors::IDTokenVerificationFailed,
+              Errors::AccessTokenRequestFailed,
+              with: :unauthorized
+
+  rescue_from Errors::UnprocessableEntity,
+              with: :unprocessable_entity
+
+  def unauthorized
+    render json: {
+      status: "error"
+    }, status: 401
+  end
+
+  def unprocessable_entity
+    render json: {
+      status: "error"
+    }, status: 422
+  end
 end
