8c252efc209873c5cd621af80565b36d75ebc953
Uche Chilaka <uche@lar.city>
2023-08-17 18:34:30 -0400

    refactor: fix for loading of feature flag defaults

--- config/initializers/features.rb
+++ config/initializers/features.rb
@@ -1,3 +1,10 @@
-# [:feat__apple_login].each do |feat_name|
-#   Flipper.enable(feat_name) unless Flipper.exist?(feat_name)
-# end
+require File.expand_path("../../../lib/feature_flag_utils", __FILE__)
+
+Flipper.configure do |config|
+  config.default do
+    adapter = Flipper::Adapters::ActiveRecord.new
+    Flipper.new(adapter)
+  end
+
+  FeatureFlagUtils.setup_defaults if FeatureFlagUtils.initialized?
+end
