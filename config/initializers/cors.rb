fdd01eec310c9e92f6bbc484ac14505c30600ecb
Uche Chilaka <uche@lar.city>
2023-08-18 09:24:57 -0400

    refactor: regex in CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,17 +3,14 @@
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
-# Read more: https://github.com/cyu/rack-cors
+# Read more: https://github.com/cyu/rack-cors#rack-configuration
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins %w[
-      localhost:3006
-      https://storyteller-ui.vercel.app
-      https://storysprout.ngrok.app
-      https://www.storysprout.app
-      https://storysprout.app
-    ]
+    origins 'localhost:3006',
+            'https://storyteller-ui.vercel.app',
+            'https://storysprout.ngrok.app',
+            /\Ahttps?:\/\/(?:[a-zA-Z0-9-]+\.)?storysprout\.app\z/
     resource "*",
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
