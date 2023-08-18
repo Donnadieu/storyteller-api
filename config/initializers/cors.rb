3749355c4d01eb1ca4435b48a5d6643eabdd1c59
Uche Chilaka <uche@lar.city>
2023-08-18 09:26:56 -0400

    SAVEPOINT

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,6 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Regex test: https://regex101.com/r/oqv0X5/1
     origins 'localhost:3006',
+            '127.0.0.1:3006',
             'https://storyteller-ui.vercel.app',
             'https://storysprout.ngrok.app',
             /\Ahttps?:\/\/(?:[a-zA-Z0-9-]+\.)?storysprout\.app\z/
