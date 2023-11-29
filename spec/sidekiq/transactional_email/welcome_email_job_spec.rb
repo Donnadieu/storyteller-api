a306c85e209b3a679f939d73c53f7b9fca111e70
Uche Chilaka <uche@lar.city>
2023-11-29 06:11:59 -0500

    SAVEPOINT

--- spec/sidekiq/transactional_email/welcome_email_job_spec.rb
+++ spec/sidekiq/transactional_email/welcome_email_job_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+# Doc on testing rails jobs: https://guides.rubyonrails.org/v7.0/testing.html#testing-jobs
 RSpec.describe TransactionalEmail::WelcomeEmailJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
