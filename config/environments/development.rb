2ecbd5d8bf2503a0bae90fdfff4b2919668b1b34
Uche Chilaka <uche@lar.city>
2023-12-21 09:05:57 -0500

    refactor: fix correct configuration for exceptions on missing translations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -98,13 +98,10 @@ Rails.application.configure do |app|
   }
 
   # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Fail loudly when missing translations occur.
-  config.action_view.raise_on_missing_translations = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -55,11 +55,8 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation_warnings = []
 
   # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Fail loudly when missing translations occur.
-  config.action_view.raise_on_missing_translations = true
 end
